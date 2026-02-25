6f2d745b9c08a746b6e219a3929d3e7c75372130
Alejo21121 <toni2sas@gmail.com>
2026-02-25 16:10:39 -0500

    contraseña

--- resources/views/profile/change-password.blade.php
+++ resources/views/profile/change-password.blade.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet" href="{{ asset('css/abogado.css') }}">
     <link rel="stylesheet" href="{{ asset('css/profile.css') }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script> {{-- 👈 AGREGAR --}}
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     @vite(['resources/js/dash.js'])
     <title>Cambiar Contraseña</title>
 </head>
